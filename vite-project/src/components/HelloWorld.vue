7cea2a682b2cb9575e07a62cf35ea4d5a5dc737b
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-12-04 10:50:32 +0600

    routing starting

--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -1,44 +1,29 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div class="hello-box">
+    <h1>{{ message }}</h1>
+    <button @click="changeText">Click Me</button>
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello Vue from Ashik!"
+    }
+  },
+  methods: {
+    changeText() {
+      this.message = "Button Clicked! Vue is Working!"
+    }
   }
+}
+</script>
+
+<style>
+.hello-box {
+  padding: 20px;
+  background: #f3f3f3;
+  border-radius: 10px;
 }
 </style>
