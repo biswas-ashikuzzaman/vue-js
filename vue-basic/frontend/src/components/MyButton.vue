cce7dccde49ac8c61e2c68f3a0db76e3075457db
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-12-08 10:09:18 +0600

    u

---
+++ vue-basic/frontend/src/components/MyButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="btn">Click Me</button>
+</template>
+
+<script>
+export default {
+  name: "MyButton"
+}
+</script>
+
+<style>
+.btn {
+  background: green;
+  color: white;
+  padding: 10px;
+}
+</style>
